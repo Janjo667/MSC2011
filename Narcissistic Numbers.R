f3ffd248d59ab887590245712bb08bff6106e3c5
Deweena <deweenaparija@Deweenas-MacBook-Pro.local>
2022-06-13 16:37:36 -0400

    Assignment: Narcissistic Number without using loops

---
+++ Narcissistic Numbers.R
@@ -0,0 +1,36 @@
+# Checking if user input number is narcissistic
+
+# Read User input
+n <- readline("Enter a 3 digit positive number: ")
+
+# Convert into numeric
+n <- as.numeric(n)
+
+# Check is user input is valid (numeric, positive and 3 digits)
+if (is.na(n) || n < 0 || nchar(n) != 3) {
+  print("Invalid entry: terminating program")
+} else {
+  
+  # Separate 3 digit input to individual ones
+  d <- as.numeric(strsplit(as.character(n), "")[[1]])
+  
+  #Sub-setting separated digits from n  
+  x <- d[[1]]
+  y <- d[[2]]
+  z <- d[[3]]
+
+  # calculating if n is a Narcissistic number 
+  e <- x^3 + y^3 + z^3
+  
+}
+  # If e is the same as what was inputted as n - its a narcissist number
+if (e == n) {
+  # Display the results
+  print(paste(n, "is a narcissistic number!"))
+} else {
+  print(paste(n, "is not a narcissistic number :("))
+  
+}
+
+
+
